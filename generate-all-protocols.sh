e98e1432863eb16dd4e7762fca8884ca2c4bf0c6
Vital Fadeev <vital.fadeev@gmail.com>
2025-08-11 21:24:25 +0700

    upd

--- generate-all-protocols.sh
+++ generate-all-protocols.sh
@@ -6,7 +6,7 @@ DEST=protocol
 
 ./wayland-scanner-d /usr/share/wayland/wayland.xml > $DEST/wayland.d
 
-for NAME in wayland-protocols/stable/*/*
+for NAME in /usr/share/wayland-protocols/stable/*/*
 do
   ./wayland-scanner-d $NAME > $DEST/`basename $NAME | cut -f 1 -d '.' | tr - _`.d
 done
